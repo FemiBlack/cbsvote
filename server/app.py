dce6aa7e958e1c63acee9c8de5f51aa5772c7fc1
femiBlack <56875358+FemiBlack@users.noreply.github.com>
2021-08-07 01:09:03 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -27,7 +27,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+# app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 mail = Mail(app)
 
