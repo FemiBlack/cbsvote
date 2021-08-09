17c7c3c79ae90f8fd39cbb91f07c5a3441ba39cd
femiBlack <56875358+FemiBlack@users.noreply.github.com>
2021-08-09 23:14:52 +0100

    Update auth.js

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -3,9 +3,9 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 /* eslint no-shadow: ["error", { "allow": ["state"] }] */
 const state = {
   user: {
-    id: '',
-    email: '',
-    role: ''
+    id: null,
+    email: null,
+    role: null
   },
   leads: null,
 };
